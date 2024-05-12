4dd4c16e5a5ff09766e2ecbad0cc58b828c34e61
Dan Cruz <dancruz.violin@gmail.com>
2024-05-12 15:46:18 +0200

    Creacion package Familia

---
+++ src/main/kotlin/Familia/Familia.kt
@@ -0,0 +1,7 @@
+package Familia
+
+class Familia(val id_familia: Int, val descripcion: String) {
+    override fun toString(): String {
+        return "Familia(id_familia=$id_familia, descripcion='$descripcion')"
+    }
+}
