3f520333ce6e548bd1ae257fbda180a692be8ae5
IsabelEstevezZornoza <161695824+IsabelEstevezZornoza@users.noreply.github.com>
2024-05-12 19:34:29 +0200

    modificacion

--- src/main/kotlin/Terapeuta/Terapeuta.kt
+++ src/main/kotlin/Terapeuta/Terapeuta.kt
@@ -3,6 +3,7 @@ package Terapeuta
 class Terapeuta(val id_terapeuta:Int, val nombre:String, val apellido1:String, val apellido2:String, val id_metodologia:Int) {
 
     override fun toString(): String {
+        var terapeuta:String
         return "Terapeuta(id_terapeuta=$id_terapeuta, nombre='$nombre', apellido1='$apellido1', apellido2='$apellido2', id_metodologia=$id_metodologia)"
     }
 }
