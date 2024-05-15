8a3bf317de8ce18f71f67f1f5214dddd61416bda
Dan Cruz <dancruz.violin@gmail.com>
2024-05-15 20:14:03 +0200

    Test Menu

---
+++ src/test/kotlin/Menu/MenuTest.kt
@@ -0,0 +1,13 @@
+package Menu
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MenuTest {
+
+    @Test
+    fun pedirID() {
+        assertEquals(1,1,"El test ha fallado.")
+    }
+}
