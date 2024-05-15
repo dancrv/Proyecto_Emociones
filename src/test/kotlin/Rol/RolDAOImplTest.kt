7c45018bd074ba6d0ef78eb91ca136075029a400
Dan Cruz <dancruz.violin@gmail.com>
2024-05-15 20:21:55 +0200

    Test Rol

---
+++ src/test/kotlin/Rol/RolDAOImplTest.kt
@@ -0,0 +1,36 @@
+package Rol
+
+import Metodologia.Metodologia
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class RolDAOImplTest {
+    var rol = Metodologia(1,"Madre")
+    var roles = ArrayList<Rol>()
+
+    @Test
+    fun buscarRol() {
+        assertEquals(rol,rol,"El test ha fallado.")
+    }
+
+    @Test
+    fun sacarRol() {
+        assertEquals(roles,roles,"El test ha fallado.")
+    }
+
+    @Test
+    fun insertarRol() {
+        assertTrue(true,"El test ha fallado.")
+    }
+
+    @Test
+    fun modificarRol() {
+        assertTrue(true,"El test ha fallado.")
+    }
+
+    @Test
+    fun borrarRol() {
+        assertTrue(true,"El test ha fallado.")
+    }
+}
