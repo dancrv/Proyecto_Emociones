cd2f0ea0406a27d139a21d296ab3de852dfd58ec
IsabelEstevezZornoza <161695824+IsabelEstevezZornoza@users.noreply.github.com>
2024-05-16 21:00:52 +0200

    Documentación interfaz EmocionDAO y FamiliaDAO

--- src/main/kotlin/Emocion/EmocionDAO.kt
+++ src/main/kotlin/Emocion/EmocionDAO.kt
@@ -1,9 +1,47 @@
 package Emocion
 
+/**
+ * Interfaz para acceder a los datos de las emociones.
+ *
+ * @constructor Create empty Emocion d a o
+ */
 interface EmocionDAO {
+    /**
+     * La función emoción busca una emoción por su identificador unico
+     *
+     * @param id_emocion es el identificador único de la emoción a buscar.
+     * @return La emoción encontrada, o null si no se encontró ninguna emoción con el ID especificado
+     */
     fun buscarEmocion(id_emocion: Int): Emocion?
+
+    /**
+     * La función Sacar emociones Obtiene una lista de todas las emociones almacenadas
+     *
+     * @return Una lista de objetos Emocion
+     */
     fun sacarEmociones(): List<Emocion>
+
+    /**
+     * La función Insertar emoción inserta una nueva emoción en la base de datos.
+     *
+     *@param emocion es la emoción que se va a insertar.
+     *@return true si la inserción fue exitosa, false en caso contrario.
+     */
     fun insertarEmocion(emocion: Emocion): Boolean
+
+    /**
+     * La funcion Modificar emocion modifica una emoción existente en la base de datos.
+     *
+     * @param emocion es emoción con los datos actualizados que se va a modificar.
+     * @return true si la modificación fue exitosa, false en caso contrario.
+     */
     fun modificarEmocion(emocion: Emocion): Boolean
+
+    /**
+     * La funcion Borrar emocion borra una emoción de la base de datos por su identificador único
+     *
+     * @param id_emocion es el identificador único de la emoción que se va a borrar.
+     * @return true si la eliminación fue exitosa, false en caso contrario.
+     */
     fun borrarEmocion(id_emocion: Int): Boolean
 }
--- src/main/kotlin/Familia/FamiliaDAO.kt
+++ src/main/kotlin/Familia/FamiliaDAO.kt
@@ -1,9 +1,47 @@
 package Familia
 
+/**
+ * Interfaz para acceder a los datos de las familias
+ *
+ * @constructor Create empty Familia d a o
+ */
 interface FamiliaDAO {
+    /**
+     * La funcion Buscar familia busca una familia por su identificador único.
+     *
+     * @param id_familia es el identificador único de la familia a buscar.
+     * @return devuelve La familia encontrada, o null si no se encontró ninguna familia con el ID especificado.
+     */
     fun buscarFamilia(id_familia: Int): Familia?
+
+    /**
+     * La funcion Sacar familia obtiene una lista de todas las familias almacenadas.
+     *
+     * @return devuelve una lista de familias
+     */
     fun sacarFamilia(): List<Familia>
+
+    /**
+     * La funcion Insertar familia inserta una nueva familia en el sistema
+     *
+     * @param familia es la famlia que se va a insertar
+     * @return true si la inserción fue exitosa, false en caso contrario
+     */
     fun insertarFamilia(familia: Familia): Boolean
+
+    /**
+     * La funcion Modificar familia modifica una familia existente en el sistema
+     *
+     * @param familia es la familia con los datos actualizados que se va a modificar
+     * @return true si la modificación fue exitosa, false en caso contrario
+     */
     fun modificarFamilia(familia: Familia): Boolean
+
+    /**
+     *La funcion Borrar familia borra una familia del sistema por su identificador único
+     *
+     * @param id_familia es el identificador único de la familia que se va a borrar
+     * @return true si la eliminación fue exitosa, false en caso contrario
+     */
     fun borrarFamilia(id_familia: Int): Boolean
 }
